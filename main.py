547e78f169bd3a6975445cb7e27425b849464f43
Junho Yeo <i@junho.io>
2023-10-27 03:15:08 +0900

    Use Python-tesseract(`pytesseract`)

--- main.py
+++ main.py
@@ -1,32 +1,56 @@
-import easyocr
-import subprocess
-
 from threading import Thread
 from queue import Queue
+import easyocr
+import pytesseract
 
 
-def easy_ocr():
-    reader = easyocr.Reader(["ko", "en"])
-    text = reader.readtext("demo.png", detail=0)
+def job_easy_ocr(_options):
+    reader = easyocr.Reader(_options["lang"])
+    text = reader.readtext(_options["path"], detail=0)
     text = "".join(text)
-    print(text)
+    print("[*] job_easy_ocr", text)
     return text
 
 
-def tesseract_wasm():
-    output = subprocess.check_output(["node", "dist"], shell=False).decode("utf-8")
-    output = output.replace("\n", "\\n")
-    print(output)
-    return output
+def convert_to_tesseract_lang_code(langs: list[str]) -> str:
+    # Mapping from easyocr language codes to tesseract language codes.
+    # You might need to expand this if you are using other languages.
+    mapping = {
+        "en": "eng",
+        "ko": "kor",
+    }
+    return "+".join([mapping[lang] for lang in langs if lang in mapping])
 
 
-def wrapper(func, queue):
-    queue.put(func())
+def job_tesseract_ocr(_options):
+    lang = convert_to_tesseract_lang_code(_options["lang"])
+    text = pytesseract.image_to_string(
+        _options["path"],
+        lang=lang,
+        **_options["tesseract"]
+        # pass rest of tesseract options here.
+    )
+    text = text.replace("\n", "\\n")
+    print("[*] job_tesseract_ocr", text)
+    return text
+
+
+def wrapper(func, args, queue):
+    queue.put(func(args))
 
 
 q1, q2 = Queue(), Queue()
-Thread(target=wrapper, args=(easy_ocr, q1)).start()
-Thread(target=wrapper, args=(tesseract_wasm, q2)).start()
+options = {
+    "path": "demo.png",
+    "lang": ["ko", "en"],
+    "tesseract": {
+        # tesseract config
+        "config": "--tessdata-dir ./tessdata"
+    },
+}
+
+Thread(target=wrapper, args=(job_easy_ocr, options, q1)).start()
+Thread(target=wrapper, args=(job_tesseract_ocr, options, q2)).start()
 
 q1 = q1.get()
 q2 = q2.get()
@@ -34,4 +58,6 @@ q2 = q2.get()
 prompt = f"""Combine and correct OCR results [0] and [1], using \\n for line breaks. Answer in the JSON format {{data:<output:string>}}:
 [0]: {q1}
 [1]: {q2}"""
+
+print("=====")
 print(prompt)
