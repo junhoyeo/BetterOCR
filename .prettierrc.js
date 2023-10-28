9a92f8f0de728ba1c0c4ae2ae3a9bf04b4033565
Junho Yeo <i@junho.io>
2023-10-28 14:50:25 +0900

    Use Prettier for only formatting JSON/JS files

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,9 +4,4 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   semi: true,
-
-  // @trivago/prettier-plugin-sort-imports
-  importOrder: ['<THIRD_PARTY_MODULES>', '@/(.*)$', '^[./](.*)$'],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
 };
