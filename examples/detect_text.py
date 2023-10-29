805d0b4d77fd2fdeccbfd7386e6b510ef23732b7
Junho Yeo <i@junho.io>
2023-10-29 13:16:55 +0900

    Add example scripts

--- examples/detect_text.py
+++ examples/detect_text.py
@@ -1,9 +1,9 @@
 from betterocr import detect_text
 
 text = detect_text(
-    "demo.png",
+    ".github/images/demo-1.png",
     ["ko", "en"],
-    context="",
+    context="퍼멘테이션 펩타인 아이케어 크림",  # product name
     tesseract={"config": "--tessdata-dir ./tessdata"},
     openai={
         "model": "gpt-3.5-turbo",
