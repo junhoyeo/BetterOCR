0290bb2c7d47d5a741b362ee524056d6a7e5db72
Junho Yeo <i@junho.io>
2023-11-01 23:50:03 +0900

    Use output as text in `job_easy_pororo_ocr`

--- betterocr/ocr_engines/easy_pororo_ocr.py
+++ betterocr/ocr_engines/easy_pororo_ocr.py
@@ -5,6 +5,10 @@ def job_easy_pororo_ocr(_options):
     image = load_with_filter(_options["path"])
     m_ocr = EasyPororoOcr()
     text = m_ocr.run_ocr(image, debug=False)
+
+    if isinstance(text, list):
+        text = "\\n".join(text)
+
     print("[*] job_easy_pororo_ocr", text)
     return text
 
